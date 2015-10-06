6962bc0e2d49805afd1fc7ea7f92ee2e4af503bd
Dinakar Tumu <dinakar91@gmail.com>
2015-10-06 16:22:25 -0400

    Created .gitignore file

--- js/custom.js
+++ js/custom.js
@@ -3,20 +3,7 @@
  */
 
 $(document).ready(function(){
-    //$(".nav_profile_img").click(function(){
-    //    $( ".profile_options" ).toggleClass( "profile_options_show" );
-    //    $( ".profile_options" ).toggleClass( "slideInLeft" );
-    //    $( ".first .second" ).toggleClass( "fadeInUp" );
-    //
-    //    var count = $(".first .second").length;
-    //    for (var i=1; i<=count;i++){
-    //        var a=(i/20+"s");
-    //
-    //        $( ".first .second:nth-child("+ i +")" ).css("animation-delay",a);
-    //
-    //    }
-    //
-    //});
+
 
     $(".test1").click(function(){
         $( ".profile_options" ).toggleClass( "profile_options_show" );
@@ -33,14 +20,4 @@ $(document).ready(function(){
 
     });
 
-
 });
-//
-//$(document).ready(function(){
-//    $(" .test").click(function(){
-//        // alert("The paragraph was clicked.");
-//        console.log("hello");
-//    });
-//
-//    console.log("hello");
-//});
