de779a8b9adf06b764864af826850e769fb58bd4
EricRho <eric.rho.87@gmail.com>
2015-10-07 16:29:23 -0400

    Fix titles

--- js/account.js
+++ js/account.js
@@ -1,39 +1,39 @@
 $( document ).ready(function() {
 
 	$( ".at1" ).click(function() {
-	 
+
 		$('.at1').addClass('activated');
 		$('.at2, .at3, .at4').removeClass('activated');
 
 		$('.tab_1').removeClass('hide');
-		$('.tab_2, .tab_3, .tab_4').addClass('hide');		
+		$('.tab_2, .tab_3, .tab_4').addClass('hide');
 	});
 
 	$( ".at2" ).click(function() {
-	 
+
 		$('.at2').addClass('activated');
 		$('.at1, .at3, .at4').removeClass('activated');
 
 		$('.tab_2').removeClass('hide');
-		$('.tab_1, .tab_3, .tab_4').addClass('hide');		
+		$('.tab_1, .tab_3, .tab_4').addClass('hide');
 	});
 
 	$( ".at3" ).click(function() {
-	 
+
 		$('.at3').addClass('activated');
 		$('.at2, .at1, .at4').removeClass('activated');
 
 		$('.tab_3').removeClass('hide');
-		$('.tab_2, .tab_1, .tab_4').addClass('hide');		
+		$('.tab_2, .tab_1, .tab_4').addClass('hide');
 	});
 
 	$( ".at4" ).click(function() {
-	 
+
 		$('.at4').addClass('activated');
 		$('.at2, .at3, .at1').removeClass('activated');
 
 		$('.tab_4').removeClass('hide');
-		$('.tab_2, .tab_3, .tab_1').addClass('hide');		
+		$('.tab_2, .tab_3, .tab_1').addClass('hide');
 	});
 
 });
