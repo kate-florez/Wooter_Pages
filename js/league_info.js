a6dc40fb2fa12da9db1ea09d29c9ae15c96d0a9c
EricRho <eric.rho.87@gmail.com>
2015-10-08 14:10:22 -0400

    Styling almost done. Finish 'add' buttons

---
+++ js/league_info.js
@@ -0,0 +1,22 @@
+// $(document).ready(function() {
+//   $('.main_img').hover(function() {
+//     $(this).addClass('hover');
+//   }, function() {
+//     $(this).removeClass('hover');
+//   });
+// });
+
+
+$(document).ready(function() {
+  $('.main_img').hover(function() {
+    $(this).addClass('hover');
+  }, function() {
+    $(this).removeClass('hover');
+  });
+
+  $('.delete_image').hover(function() {
+    $(this).addClass('delete_hover');
+  }, function() {
+    $(this.removeClass('delete_hover'));
+  });
+});
