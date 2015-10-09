5df7cbcc5d035696200adf532d1667425adbed27
Dinakar Tumu <dinakar91@gmail.com>
2015-10-09 16:32:29 -0400

    Changed class names for repsonsive header

--- js/custom.js
+++ js/custom.js
@@ -8,24 +8,25 @@ $(document).ready(function(){
         $( ".hamburger_menu_list" ).removeClass( "profile_options_show" );
         $( ".hamburger_menu_list" ).removeClass( "slideInLeft" );
         $( ".hamburger_menu_list .first .second" ).removeClass( "fadeInUp" );
-        $( ".profile_options1" ).toggleClass( "profile_options_show" );
-        $( ".profile_options1" ).toggleClass( "slideInLeft" );
-        $( ".profile_options1 .first .second" ).toggleClass( "fadeInUp" );
 
-        var count = $(".profile_options1 .first .second").length;
+        $( ".profile_options_list" ).toggleClass( "profile_options_show" );
+        $( ".profile_options_list" ).toggleClass( "slideInLeft" );
+        $( ".profile_options_list .first .second" ).toggleClass( "fadeInUp" );
+
+        var count = $(".profile_options_list .first .second").length;
         for (var i=1; i<=count;i++){
             var a=(i/20+"s");
 
-            $( ".profile_options1 .first .second:nth-child("+ i +")" ).css("animation-delay",a);
+            $( ".profile_options_list .first .second:nth-child("+ i +")" ).css("animation-delay",a);
 
         }
 
     });
 
     $(".hamburger_menu_button").click(function(){
-        $( ".profile_options1" ).removeClass("profile_options_show"  );
-        $( ".profile_options1" ).removeClass( "slideInLeft" );
-        $( ".profile_options1 .first .second" ).removeClass( "fadeInUp" );
+        $( ".profile_options_list" ).removeClass("profile_options_show"  );
+        $( ".profile_options_list" ).removeClass( "slideInLeft" );
+        $( ".profile_options_list .first .second" ).removeClass( "fadeInUp" );
 
         $( ".hamburger_menu_list" ).toggleClass( "profile_options_show" );
         $( ".hamburger_menu_list" ).toggleClass( "slideInLeft" );
